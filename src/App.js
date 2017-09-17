64f62ca545a4d1a3e74c3d14a8505d6defb55aa8
kotryn <kotry1415n@gmail.com>
2017-09-17 11:47:57 +0200

    new component Counter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import App2 from './App2'
+import Counter from './Counter'
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state={
-            counter: 0,
-            list: ['a', 'b', 'c']
+            counter: 0
         }
     }
-    click = () => {this.setState({counter: this.state.counter+1})}
+    increment = () => {this.setState({counter: this.state.counter+1})}
+    decrement = () => {this.setState({counter: this.state.counter-1})}
   render() {
         console.log(this.state);
         const {name} = this.props;
-        const list = this.state.list;
-        const items = list.map((item, index) => (
-            <ul key={index}>{item}</ul>
-        ));
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+
         <p className="App-intro">
           Hi {name}: {this.state.counter}
         </p>
-          <button onClick={this.click}>+</button>
-          <App2 name={'Test'}/>
-          <div>{items}</div>
+
+          <Counter name="Increment" func={this.increment}/> <Counter name="Decrement" func={this.decrement}/>
       </div>
     );
   }
